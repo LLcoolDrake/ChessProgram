a5d3f52a586b0d0b7d85c2df6a15f233ee102d58
LLcoolDrake <LLcoolDrake@users.noreply.github.com>
2020-04-26 14:04:13 -0400

    Add files via upload

--- ChessGUI.py
+++ ChessGUI.py
@@ -1,13 +1,103 @@
+import sys
 import pygame
+
 
 class GUI:
 
 
   GameName = "chess"
+  screen = None
 
   def __init__(self):
     pygame.init()
-    screen = pygame.display.set_mode((400,400))
-    my_image = pygame.image.load("ChessBoard.png")  
-    screen.blit(my_image,(0,0))
-    pygame.display.flip()
+    self.screen = pygame.display.set_mode((400, 400))
+    pygame.display.set_caption("CS 205 Chess")
+
+    self.my_image = pygame.image.load("ChessBoard.png").convert()
+    self.screen.blit(self.my_image,(0,0))
+
+
+  def moveClick(self):
+      move_cords = []
+      while len(move_cords) < 4:
+        event = pygame.event.wait()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            cords = pygame.mouse.get_pos()
+            move_cords.append(cords[0] // 50)
+            move_cords.append(cords[1] // 50)
+            print("hi")
+            print(move_cords)
+        event2 = pygame.event.wait()
+        if event2.type == pygame.MOUSEBUTTONDOWN:
+            cords = pygame.mouse.get_pos()
+            move_cords.append(cords[0] // 50)
+            move_cords.append(cords[1] // 50)
+            print("hey")
+            print(move_cords)
+      return move_cords
+
+  def update_screen(self, board):
+    self.screen.blit(self.my_image, (0, 0))
+
+    for row in range(8):
+        for col in range(8):
+            if board.Board[row][col] == "wP":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("whitePawn.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "wR":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("whiteRook.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "wK":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("whiteKnight.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "wB":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("whiteBishop.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "wQ":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("whiteQueen.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "wKK":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("whiteKing.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "bP":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("blackPawn.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "bR":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("blackRook.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "bK":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("blackKnight.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "bB":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("blackBishop.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "bQ":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("blackQueen.png")
+                self.screen.blit(img, (x, y))
+            elif board.Board[row][col] == "bKK":
+                (x, y) = self.chess_to_screen((row, col))
+                img = pygame.image.load("blackKing.png")
+                self.screen.blit(img, (x, y))
+
+        pygame.display.update()
+  def chess_to_screen(self, chess_coord):
+      (row, col) = chess_coord
+      x = 12 + col * 50
+      y = row * 50
+      return (x, y)
+  def screen_to_chess(self, screen_coord):
+      (x, y) = screen_coord
+      row = y/50
+      col = x/50
+      return (row, col)
